f0a6cfe7ccc8188f0c26a69bf356b9818cb0e5b0
salurs <orhsssalur@gmail.com>
2018-10-09 04:40:21 +0300

    newUser

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,12 +1,23 @@
 app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFactory) =>{
-    const connOpt = {
-        reconnectionAttempts: 5,
-        reconnectionDelay: 500,
-    };
-    indexFactory.connectSocket('http://localhost:3000', connOpt)
-    .then((socket) =>{
-        console.log('Bağlantı gerçekleşti', socket);
-    }).catch((error) =>{
-        console.log(error);
-    });
+    
+    $scope.init = () =>{
+        const username = prompt('Please Enter Username');
+        if(username)
+            initSocket(username);
+        else
+            return false;
+    }
+
+    function initSocket(username){
+        const connOpt = {
+            reconnectionAttempts: 5,
+            reconnectionDelay: 500,
+        };
+        indexFactory.connectSocket('http://localhost:3000', connOpt)
+        .then((socket) =>{
+            socket.emit('newUser', {username: username});
+        }).catch((error) =>{
+            console.log(error);
+        });
+    }
 }]);
