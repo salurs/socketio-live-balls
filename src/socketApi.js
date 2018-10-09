f3021a93b0fb6f2c78c624fb0128a4dfb5ab1ad0
salurs <orhsssalur@gmail.com>
2018-10-09 05:34:31 +0300

    newUser

--- src/socketApi.js
+++ src/socketApi.js
@@ -4,7 +4,7 @@ const io = socketio();
 const socketApi = {};
 socketApi.io = io;
 
-const users = [];
+const users = {};
 
 io.on('connection', (socket) =>{
     console.log('a user connected');
@@ -15,9 +15,13 @@ io.on('connection', (socket) =>{
            position: {x: 0, y: 0}
        }
        const userData = Object.assign(data, defaultData);
-       users.push(userData);
+       users[socket.id] = userData;
 
-       socket.broadcast.emit('newUser', userData);
+       socket.broadcast.emit('newUser', users[socket.id]);
+    });
+
+    socket.on('disconnect', () =>{
+        socket.broadcast.emit('disUser', users[socket.id]);
     });
 });
 
